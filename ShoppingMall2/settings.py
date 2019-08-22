bd42de5d2a8075727fa0a26b4cbe9f563a3633fc
songchip <scv487@naver.com>
2019-08-22 15:16:37 +0900

    database settings

--- ShoppingMall2/settings.py
+++ ShoppingMall2/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'ShoppingMall2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'point_mall',
+        'USER': 'root',
+        'PASSWORD': 'clqls4013*',
+        'HOST': 'point-mall.cbsoxrvgvaj7.us-east-1.rds.amazonaws.com',
+        'PORT': '3306'
     }
 }
 
